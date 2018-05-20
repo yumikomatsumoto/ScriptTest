119e8a7aadabd7b77ba92778add7a9e059899927
yumikomatsumoto <matsumoto.0124.dm@gmail.com>
2018-05-20 22:42:52 +0900

    ScriptTestプロジェクト　課題
    Lesson4の課題ファイルです。(課題と発展課題がまとめて入っています)

---
+++ Assets/Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss {
+    private int mp = 53;
+    
+    //魔法攻撃用の関数
+    public void Magic() {
+        mp = mp - 5;
+        
+        if (mp >= 5){
+            Debug.Log("魔法攻撃をした。残りMPは" + mp);
+
+        }else{
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+                     
+    }
+}
+
+public class Test : MonoBehaviour {
+
+    void Start() {
+        int[] array = { 55, 51, 5, 24, 3 };
+
+        //配列の要素数の分だけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+        {
+            //配列の要素を表示する
+            Debug.Log(array[i]);
+        }
+
+        //配列を逆順に並べる
+        for (int i = array.Length - 1; 0 <= i; i--)
+        {
+            //配列を表示する
+            Debug.Log(array[i]);
+        }
+
+        //Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        for (int i = 0; i < 10; i++) {
+            //魔法攻撃用の関数を呼び出す
+            lastboss.Magic();
+        }
+    }
+}
